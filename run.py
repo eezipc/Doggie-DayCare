31ceb4a3888f3cc3d97813014635d9829bf5791a
Alan Staunton <eezipc@gmail.com>
2020-08-13 11:03:41 +0000

    Indented code and added comments

--- run.py
+++ run.py
@@ -1,27 +1,29 @@
+
+
 import os
 import json
 from flask import Flask, render_template, redirect, request, url_for, session, flash, Markup
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-#import bcrypt
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
+#Connect Database
 app.config['MONGO_DBNAME'] = 'doggiedatabase'
 app.config['MONGO_URI'] = 'mongodb+srv://doggieuser:doggiepassword@doggiecluster.wtitg.mongodb.net/doggiedatabase?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
-
+#Index Page
 @app.route('/')
 def index():
-  
     data = []
     with open("data/doggie.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("index.html", page_title="Doggie Daycare", doggie=data)
 
+#About Page
 @app.route('/about')
 def about():
     data = []
@@ -29,6 +31,7 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="Doggie About")
 
+#Contact Page
 @app.route('/contact', methods=['POST', 'GET'])
 def contact():
     data = []
@@ -36,6 +39,7 @@ def contact():
         data = json.load(json_data)
     return render_template("contact.html", page_title="Doggie Contact")
 
+#Daycare Page
 @app.route('/daycare')
 def daycare():
     data = []
@@ -43,6 +47,7 @@ def daycare():
         data = json.load(json_data)
     return render_template("daycare.html", page_title="Doggie Daycare", doggie=data) 
 
+#Grooming Page
 @app.route('/grooming', methods=['POST', 'GET'])
 def grooming():
     data = []
@@ -50,14 +55,14 @@ def grooming():
         data = json.load(json_data)
     return render_template("grooming.html", page_title="Doggie Grooming", doggie=data)
     
-
+#Insert Booking Page
 @app.route('/insert_booking', methods=['POST'])
 def insert_booking():
     doggiebook = mongo.db.doggiebook
     doggiebook.insert_one(request.form.to_dict())
     return redirect(url_for('confirm'))
 
-
+#Confirmation Page
 @app.route('/confirm')
 def confirm():
     data = []
@@ -66,19 +71,15 @@ def confirm():
     return render_template("confirm.html", page_title="Doggie Confirmation", doggie=data)
 
 
-#########################################
-
-# REGISTER------------------------------------
+#Register Page
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         existing_user = mongo.db.doggielogin.find_one(
             {"email_address": request.form.get("email_address").lower()})
-
         if existing_user:
             flash("Email Address is already registered")
             return redirect(url_for("register"))
-
         register = {
             "email_address": request.form.get("email_address").lower(),
             "password": generate_password_hash(request.form.get("password")),
@@ -87,24 +88,19 @@ def register():
             "petname": request.form.get("petname").lower()
         }
         mongo.db.doggielogin.insert_one(register)
-
-         # put the user in session cookie
         session["user"] = request.form.get("email_address").lower()
         flash("Your Account Has Been Registered")
         return redirect(url_for("profile", email_address=session["user"]))
     return render_template("register.html", page_title="Doggie Register")
 
-# LOGIN-----------------------------------------------------
-
-
+#Login Page
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
         existing_user = mongo.db.doggielogin.find_one(
             {"email_address": request.form.get("email_address").lower()})
-
         if existing_user:
-            # password match check
+            
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("email_address").lower()
@@ -113,46 +109,32 @@ def login():
                 return redirect(url_for(
                         "profile", email_address=session["user"]))
             else:
-                # invalid password
+                
                 flash("Incorrect Password. Please Try Again.")
                 return redirect(url_for("login"))
-
         else:
-            # the username is not registered
+           
             flash("Email is Not Registered")
             return redirect(url_for("login"))
     return render_template("login.html", page_title="Doggie Login")
 
-
-
-#USER'S PROFILE -----------------------
+#Profile Page
 @app.route("/profile/<email_address>", methods=['GET', 'POST'])
 def profile(email_address):
-    
-    # get the session username from db
     email_address = mongo.db.doggielogin.find_one(
        {"email_address": session["user"]})["email_address"]
-
     if session["user"]:
         return render_template("profile.html", email_address=email_address, page_title="Doggie Profile")
-
     return redirect(url_for("profile"))
 
-
-
-# LOGOUT -----------------------------------
-
+#Logout Page
 @app.route("/logout")
 def logout():
-    # remove user from current session cookie
-
     flash("You have been logged out. See you soon!")
     session.pop("user")
     return redirect(url_for("login"))
 
-
-# DELETE PROFILE ------------------------------
-
+#Delete Profile Page
 @app.route("/delete_profile/<email_address>", methods=["GET", "POST"])
 def delete_profile(email_address):
     mongo.db.doggielogin.remove({"email_address": session["user"]})
@@ -160,13 +142,7 @@ def delete_profile(email_address):
     flash("Your profile has been deleted.")
     return redirect(url_for("index"))
 
-
-
-
-##########################################
-
-
-
+#Overnight Page
 @app.route('/overnight')
 def overnight():
     data = []
@@ -174,6 +150,7 @@ def overnight():
         data = json.load(json_data)
     return render_template("overnight.html", page_title="Doggie Sleepover", doggie=data)
 
+#Prices Page
 @app.route('/prices')
 def prices():
     data = []
@@ -181,11 +158,13 @@ def prices():
         data = json.load(json_data)
     return render_template("prices.html", page_title="Doggie Prices", doggie=data)
 
+#View Booking Page
 @app.route('/viewbooking')
 def viewbooking():
     return render_template('viewbooking.html',
                            doggiebook=mongo.db.doggiebook.find(), page_title="Doggie Bookings")
 
+#Edit Booking Page
 @app.route('/edit_booking/<task_id>', methods=['POST', 'GET'])
 def edit_booking(task_id):
     the_task =  mongo.db.doggiebook.find_one({"_id": ObjectId(task_id)})
@@ -193,7 +172,7 @@ def edit_booking(task_id):
     edit_pets = mongo.db.doggiepets.find()
     return render_template('editbooking.html', task=the_task, login=edit_login, pets=edit_pets)
 
-
+#Update Booking Page
 @app.route('/update_booking/<task_id>', methods=['POST', 'GET'])
 def update_booking(task_id):
     tasks = mongo.db.doggiebook
@@ -206,11 +185,13 @@ def update_booking(task_id):
     })
     return redirect(url_for('viewbooking'))
 
+#Delete Booking Page
 @app.route('/delete_booking/<task_id>')
 def delete_booking(task_id):
     mongo.db.doggiebook.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('viewbooking'))
 
+#End of Run.py
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
