c065a44f4a71bc17185614bbf925e61c5dac86e1
Alan Staunton <eezipc@gmail.com>
2020-08-18 18:35:35 +0000

    db not working on heroku because of terrible instructions from CI

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'doggiedatabase'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 											#Connect Database
 # app.config['MONGO_DBNAME'] = 'doggiedatabase'
